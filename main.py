270d0db0f255930b23ef6a20c62a219df8db9104
saburovsamandar <saburovsamandar2004@gmail.com>
2026-07-01 14:33:35 +0500

    MEn help comandasin qostim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import asyncio
 from aiogram import  Bot , Dispatcher
-from  aiogram.filters import  CommandStart
+from  aiogram.filters import  CommandStart,Command
 from  aiogram.types import  Message
 from dotenv import load_dotenv
 import os
@@ -15,6 +15,11 @@ dp = Dispatcher()
 @dp.message(CommandStart())
 async def start(message: Message):
     await message.answer('hello0')
+
+
+@dp.message(Command('help'))
+async def help(message: Message):
+    await message.answer('qanday jardem kerek')
 
 
 
